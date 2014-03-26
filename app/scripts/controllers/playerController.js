b23e289536d0007e33fd9c397019dbd17042846d
Julio Makdisse Saito <saitodisse@gmail.com>
2014-03-26 20:43:34 -0300

    controller and views ready

--- app/scripts/controllers/playerController.js
+++ app/scripts/controllers/playerController.js
@@ -5,6 +5,8 @@ define([
     'underscore',
     'backbone',
     '../views/status',
+    '../views/controls',
+    '../views/playlistView',
     '../models/audio',
     '../collections/playlist'
 ], function (
@@ -12,6 +14,8 @@ define([
     _,
     Backbone,
     StatusView,
+    ControlsView,
+    PlaylistView,
     Audio,
     Playlist
 ) {
@@ -29,6 +33,8 @@ define([
         initializeRegions: function() {
             this.jBody = $('.container');
             this.jStatusRegion = this.jBody.find('#status');
+            this.jControlsRegion = this.jBody.find('#controls');
+            this.jPlaylistRegion = this.jBody.find('#playlist');
         },
         
         initializeModels: function() {
@@ -40,14 +46,26 @@ define([
             this.statusView = new StatusView({
                 model: this.audio
             });
+
+            this.controlsView = new ControlsView({
+                model: this.audio
+            });
+
+            this.playlistView = new PlaylistView({
+                model: this.audio
+            });
         },
         
         renderViews: function() {
             this.statusView.render();
+            this.controlsView.render();
+            this.playlistView.render();
         },
         
         addViewsToDOM: function() {
             this.jStatusRegion.html(this.statusView.el);
+            this.jControlsRegion.html(this.controlsView.el);
+            this.jPlaylistRegion.html(this.playlistView.el);
         },
 
     });
