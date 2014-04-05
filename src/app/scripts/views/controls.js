109b7086205f4f29e0a1eef960b42179a6db08b3
Julio Makdisse Saito <saitodisse@gmail.com>
2014-04-05 13:55:56 -0300

    only native player

--- src/app/scripts/views/controls.js
+++ src/app/scripts/views/controls.js
@@ -20,15 +20,12 @@ define([
         className: '',
 
         events: {
-            'click .btnPlay': 'playOrPause',
             'click .btnPrev': 'previous',
             'click .btnNext': 'next'
         },
 
         initialize: function () {
             this.listenTo(this.model, 'change', this.render);
-
-            playerCommunicator.on('audio:pause', this.onPause, this);
             playerCommunicator.on('song:set', this.onSetSong, this);
            
         },
@@ -39,51 +36,21 @@ define([
             this.model.setNativeAudio(this.$el.find('#nativeAudio')[0]);
 
             //playerCommunicator.on('audio:play', this.onPlay, this);
-            this.model.audio.addEventListener('canplay', this.onCanPlay.bind(this), false);
-            this.model.audio.addEventListener('play', this.onPlay.bind(this), false);
-            this.model.audio.addEventListener('timeupdate', this.getSongTime.bind(this), false);
+            //this.model.audio.addEventListener('canplay', this.onCanPlay.bind(this), false);
+            //this.model.audio.addEventListener('play', this.onPlay.bind(this), false);
+            //this.model.audio.addEventListener('timeupdate', this.getSongTime.bind(this), false);
 
 
             this.getJqueryElements();
-            this.initializeVolumeInputRange();
+            //this.initializeVolumeInputRange();
         },
 
         getJqueryElements: function() {
-            this.jPlayGlyphicon = this.$el.find('.btnPlay > .glyphicon');
-            
-            this.jVolumeLabel = this.$el.find('#volumeSliderLabel');
-            this.jVolumeSlider = this.$el.find('#volumeSlider');
-            
-            this.jSliderLabel = this.$el.find('#timeSliderLabel');
-            this.jTimeSlider = this.$el.find('#timeSlider');
+            this.jNativeAudio = this.$el.find('#nativeAudio');
         },
 
         getSongTime: function() {
             this.updateTimeSlider();
-        },
-
-        updateTimeSlider: function() {
-            //Label
-            var currentTotalFormated = this.getFormatedCurrentTime();
-            this.jSliderLabel.text('Time: ' + currentTotalFormated);
-
-            //input range (slider)
-            this.jTimeSlider.val(this.model.getCurrentTime());
-        },
-
-        initializeTimeInputRange: function() {
-            var audioModel = this.model,
-                totalLength = audioModel.getTotalLength();
-
-            //Label
-            var currentTotalFormated = this.getFormatedCurrentTime();
-            this.jSliderLabel.text('Time: ' + currentTotalFormated);
-
-            //input range (slider)
-            this.jTimeSlider.attr('min', 0);
-            this.jTimeSlider.attr('max', totalLength);
-            this.jTimeSlider.attr('step', totalLength / 100);
-            this.jTimeSlider[0].addEventListener('input', this.timeChanged.bind(this));
         },
 
         getFormatedCurrentTime: function() {
@@ -94,14 +61,6 @@ define([
                 totalLengthFormated = prettyMinutes(totalLength);
                 
             return currentTimeFormated + ' / ' + totalLengthFormated;
-        },
-
-        initializeVolumeInputRange: function() {
-            this.jVolumeSlider.attr('min', 0);
-            this.jVolumeSlider.attr('max', 1);
-            this.jVolumeSlider.attr('step', 0.01);
-            this.jVolumeSlider[0].addEventListener('input', this.volumeChanged.bind(this));
-            this.volumeChanged();
         },
 
         playOrPause: function() {
@@ -115,44 +74,9 @@ define([
             playerCommunicator.trigger('playlist:next');
         },
 
-        volumeChanged: function() {
-            //Label
-            this.jVolumeLabel.text('Volume: ' + Math.floor(this.jVolumeSlider.val()*100) + '%');
-            playerCommunicator.trigger('audio:volume', this.jVolumeSlider.val());
-        },
-
-        timeChanged: function() {
-            playerCommunicator.trigger('audio:time', this.jTimeSlider.val());
-        },
-
         onSetSong: function(songModel) {
-            this.jPlayGlyphicon.removeClass('glyphicon-play');
-            this.jPlayGlyphicon.text('loading...');
-            this.jSliderLabel.text('Time: 0:00 / ...');
-
-            this.$el.find("#nativeAudio").attr('src', songModel.get('path'));
-            var nativeAudio = this.$el.find("#nativeAudio")[0];
-            nativeAudio.play();
-
+            this.jNativeAudio.attr('src', songModel.get('path'));
         },
-        
-        onCanPlay: function() {
-            this.jPlayGlyphicon.addClass('glyphicon-play');
-            this.jPlayGlyphicon.text('');
-
-            this.initializeTimeInputRange();
-        },
-        
-        onPlay: function() {
-            this.jPlayGlyphicon.removeClass('glyphicon-play');
-            this.jPlayGlyphicon.addClass('glyphicon-pause');
-        },
-        
-        onPause: function() {
-            this.jPlayGlyphicon.removeClass('glyphicon-pause');
-            this.jPlayGlyphicon.addClass('glyphicon-play');
-        },
-
     });
 
     return ControlsView;
