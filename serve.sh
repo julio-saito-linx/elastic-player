e3e297ff3b4d85069c0cf234e67df094e19c7a47
Julio Makdisse Saito <saitodisse@gmail.com>
2014-04-06 14:34:36 -0300

    serve.sh updated

--- serve.sh
+++ serve.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-cd src
+echo "git pull"
 git pull origin
-subl .
-grunt serve
+
+PROCESS_NUM=$(ps -ef | grep "sublime_text ." | grep -v "grep" | wc -l)
+
+if [ $PROCESS_NUM -eq 1 ];
+then
+    echo "Sublime already open. Nothing to do"
+else
+    echo "Opening Sublime Text..."
+    subl src/
+fi
+
+echo "Initializing server..."
+grunt --base src  --gruntfile src/Gruntfile.js serve
