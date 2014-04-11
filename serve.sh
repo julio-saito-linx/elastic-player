e18029fc85d0d2ac23f73e1e4661b38705e1b5dc
Julio Makdisse Saito <saitodisse@gmail.com>
2014-04-11 11:44:14 -0300

    serve

--- serve.sh
+++ serve.sh
@@ -3,15 +3,14 @@
 echo "git pull"
 git pull origin
 
-PROCESS_NUM=$(ps -ef | grep "sublime_text ." | grep -v "grep" | wc -l)
-
-if [ $PROCESS_NUM -eq 1 ];
-then
-    echo "Sublime already open. Nothing to do"
-else
-    echo "Opening Sublime Text..."
-    subl src/
-fi
+# PROCESS_NUM=$(ps -ef | grep "sublime_text ." | grep -v "grep" | wc -l)
+# if [ $PROCESS_NUM -eq 1 ];
+# then
+#     echo "Sublime already open. Nothing to do"
+# else
+#     echo "Opening Sublime Text..."
+#     subl src/
+# fi
 
 echo "Initializing server..."
 grunt --base src  --gruntfile src/Gruntfile.js serve
